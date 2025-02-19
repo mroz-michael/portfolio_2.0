c04003cf5dfaa41b21c8f6dcdf4f7a484bbcdb6c
Michael Mroz <michaelmroz01111@gmail.com>
2025-02-19 19:35:42 -0400

    Updates

--- src/app/about/page.js
+++
@@ -1,9 +0,0 @@
-import AboutMe from "../components/AboutMe";
-
-export default function About() {
-  return (
-    <main className="min-h-screen bg-gray-900 text-white flex flex-col items-center p-6">
-      <AboutMe />
-    </main>
-  );
-}
