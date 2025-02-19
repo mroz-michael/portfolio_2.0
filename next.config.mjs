fe4ba4cc21dde720b98c21e4835a61ed28551e8f
Michael Mroz <michaelmroz01111@gmail.com>
2025-02-19 17:50:49 -0400

    edit config for github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const isProd = process.env.NODE_ENV === "production";
+
+const nextConfig = {output: 'export',
+    assetPrefix: isProd ? '/mroz-michael.github.io' : '',
+    images: {
+        unoptimized: true,
+    }};
 
 export default nextConfig;
