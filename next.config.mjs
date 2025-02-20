b18873c303bf607339895a8e8fa56b67146e384b
Michael Mroz <michaelmroz01111@gmail.com>
2025-02-19 20:19:05 -0400

    edit config for gh pages

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@
 const nextConfig = {
     output: 'export',
     basePath: '/mroz-michael.github.io',
-    assetPrefix: '/mroz-michael.github.io', 
+    assetPrefix: '/mroz-michael.github.io/', 
     images: {
         unoptimized: true,
     }};
