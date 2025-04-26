058d4402dda421419e9b7c8857aa7e65bacfb70b
Michael Mroz <michaelmroz01111@gmail.com>
2025-04-26 11:25:29 -0300

    update aboutme page

--- src/app/components/AboutMe.js
+++ src/app/components/AboutMe.js
@@ -9,16 +9,16 @@ const AboutMe = () => {
         I'm a 2nd-year Computer Science student at Dalhousie always trying to hone my problem solving skills.
         I love working with <span className={highlightText}>Java, Python, and Full-Stack Web Development</span>.
         Right now I'm learning <span className={highlightText}>Next.js and Tailwind CSS</span> (I used both for this website!).
-        I also just started learning about Cloud through a workshop, <span className={highlightText}>I'm planning to take the AWS Cloud Practitioner exam this Spring!</span>
+        I also just started learning about Cloud through a workshop, <span className={highlightText}>I'm planning to take the AWS Cloud Practitioner exam this Summer!</span>
       </p>
       <p className="mt-3 text-white">
         I'm also currently working for Dalhousie as a <span className={highlightText}>Teaching Assistant</span> for Intro to Web Development,
         and a <span className={highlightText}>Marker</span> for Discrete Math.
       </p>
       <p className="mt-3 text-white">
-        Currently, I'm <span className={highlightText}>looking for a co-op for this Summer</span>. I'm hoping to take my skills to the next level, and at the 
-        same time make some meaningful connections with people and make a positive impact. 
+        I'm <span className={highlightText}>thrilled to be joining SimplyCast</span> as a co-op developer this Summer. I can't wait to get hands on experience in a professional environment.
       </p>
+      <p>Looking to the future, my biggest goal is to dive into <span className={highlightText}> Machine Learning and AI.</span></p>
       <footer>
         <h3 className="text-2xl white mt-10">
             Thanks for taking some time to check out my website :) Below are links to my GitHub and LinkedIn profiles
